2dfffe2ab2fc26f40ffb74cc981d78e1f403c6a2
Jonathan Potter <potter.jonathan.m@gmail.com>
2026-08-08 00:03:04 -0400

    rm scripts

--- scripts/check-packs.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-kitty --title "pack updates" /usr/bin/env sh -c \
-  'paru -Syu && echo Done && read && eww poll check-packages'
--- scripts/get-sinks.sh
+++
@@ -1,1 +0,0 @@
-pactl list sinks | grep -A 1 -m 1 "$(pactl get-default-sink)" | awk '/Description:/ {$1=""; print}'
