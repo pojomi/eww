477b5939fe6465b8d16e6eb0d5a8b75dc02013da
Jonathan Potter <potter.jonathan.m@gmail.com>
2026-08-08 19:45:34 -0400

    update cleaner jq

--- scripts/eww-dunst-json.sh
+++ scripts/eww-dunst-json.sh
@@ -3,13 +3,13 @@ dunst_json() {
   local js
   js=$(dunstctl history | jq -c '[
     .data[][]
-    | select(.urgency.data == "NORMAL")
-    | {app: .appname.data, body: (.message.data|gsub("<[^>]*>"; "")), id: .id.data}
-  ]')
-  eww u dunst-notifs="$js"
-  eww u dunst-open=true
-  eww u have-notifications=false
-  eww open dunst
+    | {app: .appname.data, body: (.message.data|gsub("<[^>]*>"; "")), id: .id.data, count: ([.appname,.message]|length-1)}
+    ]')
+  eww u dunst="$js"
+  eww u dunst.is_open=true
+  eww u dunst.has_notifications=false
+  eww u bell=""
+  eww open dunst-win
 }
 
 dunst_json
