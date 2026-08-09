c955c99c9562107ddc19591ec5e0446a89aac74d
Jonathan Potter <potter.jonathan.m@gmail.com>
2026-08-08 22:31:34 -0400

    update tracking method for notification count

--- scripts/eww-dunst-json.sh
+++ scripts/eww-dunst-json.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 dunst_json() {
   local js
-  js=$(dunstctl history | jq -c '[
-    .data[][]
-    | {app: .appname.data, body: (.message.data|gsub("<[^>]*>"; "")), id: .id.data, count: ([.appname,.message]|length-1)}
-    ]')
+  js=$(dunstctl history | jq -c '[ .data[][] | {
+    app: .appname.data, 
+    body: (.message.data|gsub("<[^>]*>"; "")), 
+    id: .id.data
+    }]')
+  local count="$(dunstctl count history)"
   eww u dunst="$js"
+  eww u notif_count="$count"
   eww u dunst.is_open=true
   eww u dunst.has_notifications=false
   eww u bell=""
